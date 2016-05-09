45ce7a670b8ecb5b1aa9efa1ed626fc3f97084a8
meihuakaile <ielichen@foxmail.com>
2016-05-09 23:51:27 +0800

    in

--- initgame.py
+++ initgame.py
@@ -3,6 +3,8 @@ from pygame.locals import *
 
 pygame.init()
 width, height = 640, 480
+keys = [False, False, False, False]
+playerpos = [100, 100]
 screen = pygame.display.set_mode((width, height))
 player = pygame.image.load("./resources/images/dude.png")
 grass = pygame.image.load("./resources/images/grass.png")
@@ -17,9 +19,20 @@ while True:
 	screen.blit(castle, (0, 135))
 	screen.blit(castle, (0, 240))
 	screen.blit(castle, (0, 345))
-	screen.blit(player, (100, 100))
+	screen.blit(player, playerpos)
 	pygame.display.flip()
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			exit(0)
+		if event.type == pygame.KEYDOWN:
+			if event.key == K_w:
+				keys[0] = True
+			elif event.key == K_a:
+				keys[1] = True
+			elif event.key == K_s:
+				keys[2] = True
+			elif event.key == K_d:
+				keys[3] = True
+		elif event.type == pygame.KEYUP:
+			if event.key == 
